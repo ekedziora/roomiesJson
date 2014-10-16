0550c6358d09199d042583f7e8f4d00bd4a6e4a7
ekedziora <ekedziora13@gmail.com>
2014-10-16 18:28:30 +0200

    Nowy obiekt dla parametrów requestu o auth code

---
+++ src/main/java/pl/kedziora/emilek/json/objects/AuthCodeRequestParams.java
@@ -0,0 +1,33 @@
+package pl.kedziora.emilek.json.objects;
+
+public class AuthCodeRequestParams {
+
+    private String mail;
+
+    private String authCode;
+
+    public AuthCodeRequestParams() {
+    }
+
+    public AuthCodeRequestParams(String mail, String authCode) {
+        this.mail = mail;
+        this.authCode = authCode;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getAuthCode() {
+        return authCode;
+    }
+
+    public void setAuthCode(String authCode) {
+        this.authCode = authCode;
+    }
+
+}
